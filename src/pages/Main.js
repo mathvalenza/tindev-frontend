e971a6f61ea0aa8564879ff35d44edf5b9388748
Valenza <matheusvvalenza@gmail.com>
2020-01-05 21:09:19 -0300

    added like and dislike features with auto reload in users list

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,91 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Main.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
 
 export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: { user: match.params.id }
+      }); 
+      
+      setUsers(response.data);
+    }    
+
+    loadUsers();
+  }, [match.params.id])
+
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: {
+        user: match.params.id,
+      },
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: {
+        user: match.params.id,
+      },
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
   return (
     <div className="main-container">
-      <img src={logo} alt="TinDev"/>
-      <ul>
-        <li>
-          <img src="https://avatars0.githubusercontent.com/u/34667046?v=4" alt=""/>
-          <footer>
-            <strong>Matheus Valenza</strong>
-            <p>
-              BRBABRRABABRABRA
-            </p>
-          </footer>
+      <Link to="/">
+        <img src={logo} alt="TinDev"/>
+      </Link>
+        {
+        users.length > 0 ? (
+            <ul>
+                {users.map(user => (
+                <li key={user._id}>
+                  <img src={user.avatar} alt={user.name}/>
+                  <footer>
+                    <strong>{user.name}</strong>
+                    <p>
+                      {user.bio}
+                    </p>
+                  </footer>
 
-          <div className="buttons">
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars0.githubusercontent.com/u/34667046?v=4" alt=""/>
-          <footer>
-            <strong>Matheus Valenza</strong>
-            <p>
-              BRBABRRABABRABRA
-            </p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars0.githubusercontent.com/u/34667046?v=4" alt=""/>
-          <footer>
-            <strong>Matheus Valenza</strong>
-            <p>
-              BRBABRRABABRABRA
-            </p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars0.githubusercontent.com/u/34667046?v=4" alt=""/>
-          <footer>
-            <strong>Matheus Valenza</strong>
-            <p>
-              BRBABRRABABRABRA
-            </p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-          </div>
-        </li>
-      </ul>
+                  <div className="buttons">
+                    <button type="button" onClick={() => handleLike(user._id)}>
+                      <img src={like} alt="Like" />
+                    </button>
+                    <button type="button" onClick={() => handleDislike(user._id)}>
+                      <img src={dislike} alt="Dislike" />
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : <div className="empty"> Acabou :( </div>
+        }
     </div>
   ) 
 }
