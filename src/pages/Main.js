65dbf6deb123c4f0a33ef358bb0c55410c50b9f6
Valenza <matheusvvalenza@gmail.com>
2020-03-18 22:28:57 -0300

    added socket connection

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Main.css';
 
@@ -21,6 +22,12 @@ export default function Main({ match }) {
     }    
 
     loadUsers();
+  }, [match.params.id])
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333', {
+      query: { user: match.params.id },
+    });
   }, [match.params.id])
 
   async function handleLike(id) {
